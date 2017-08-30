50db95cca51d48c4feb6683d6b1da1626e2cfeb5
dtmoodie <dtmoodie@gmail.com>
2017-08-30 13:55:53 +0000

    pascal fixes

--- make/config.mk
+++ make/config.mk
@@ -44,7 +44,7 @@ ADD_CFLAGS =
 #---------------------------------------------
 
 # whether use CUDA during compile
-USE_CUDA = 0
+USE_CUDA = 1
 
 # add the path to CUDA library to link and compile flag
 # if you have already add them to environment variable, leave it as NONE
@@ -52,10 +52,10 @@ USE_CUDA = 0
 USE_CUDA_PATH = NONE
 
 # whether use CuDNN R3 library
-USE_CUDNN = 0
+USE_CUDNN = 1
 
 # whether use cuda runtime compiling for writing kernels in native language (i.e. Python)
-USE_NVRTC = 0
+USE_NVRTC = 1
 
 # whether use opencv during compilation
 # you can disable it, however, you will not able to use
@@ -154,7 +154,7 @@ EXTRA_OPERATORS =
 #----------------------------
 
 # Create C++ interface package
-USE_CPP_PACKAGE = 0
+USE_CPP_PACKAGE = 1
 
 #----------------------------
 # plugins
